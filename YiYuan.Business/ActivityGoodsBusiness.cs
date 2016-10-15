7d8facac82301a666867e9229967948959a8c2fe
HuJiaming <hujiaming@szpayu.com>
2016-10-15 16:58:04 +0800

    新增活动商品业务类

---
+++ YiYuan.Business/ActivityGoodsBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YiYuan.Entity;
+
+namespace YiYuan.Business
+{
+    /// <summary>
+    /// 活动商品业务类
+    /// </summary>
+    public class ActivityGoodsBusiness : BaseBusiness<ActivityGoods>
+    {
+    }
+}
