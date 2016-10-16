8ef4df6f5b6db68446fb20562133d4911ed69d64
Mark <Mark@DESKTOP-FTRH757>
2016-10-16 21:16:11 +0800

    更新活动控制器

--- YiYuan.Web.Admin/Controllers/ActivityController.cs
+++ YiYuan.Web.Admin/Controllers/ActivityController.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using YiYuan.Entity;
-
+using YiYuan.Business;
 namespace YiYuan.Web.Admin.Controllers
 {
+    /// <summary>
+    /// 活动
+    /// </summary>
     public class ActivityController : Controller
     {
+        readonly ActivityBusiness _activityBusiness;
+
+        readonly ActivityGoodsBusiness _activityGoodsBusiness;
+
+        public ActivityController()
+        {
+            _activityBusiness = new ActivityBusiness();
+
+            _activityGoodsBusiness = new ActivityGoodsBusiness();
+        }
+
         /// <summary>
         /// 活动列表
         /// </summary>
@@ -19,14 +33,24 @@ namespace YiYuan.Web.Admin.Controllers
 
             list.Add(new Activity());
 
-
-
-
             return View(list);
         }
 
+        /// <summary>
+        /// 创建活动
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Create()
         {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Activity activitys)
+        {
+            _activityBusiness.Add(activitys, true);
 
             return View();
         }
@@ -41,5 +65,34 @@ namespace YiYuan.Web.Admin.Controllers
         {
             return View();
         }
+
+        /// <summary>
+        /// 活动添加商品
+        /// </summary>
+        /// <param name="activityId"></param>
+        /// <returns></returns>
+        public ActionResult AddGodds(int activityId)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 保存活动添加商品
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddGodds(List<ActivityGoods> list)
+        {
+            //var list = new ActivityGoods();
+            _activityGoodsBusiness.Add(list);
+
+            return View();
+        }
+
+
+
+
+
     }
 }
